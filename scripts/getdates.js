95de127fad0dbead80e6b3916fa0e1bc96ac967b
j-w-wright <wri24011@byui.edu>
2025-01-18 16:21:51 -0700

    added javascript for date

---
+++ scripts/getdates.js
@@ -0,0 +1,8 @@
+const date = new Date();
+const currentYear = date.getFullYear();
+document.getElementById("currentYear").innerHTML = currentYear;
+
+
+const lastModified = new Date(document.lastModified);
+document.getElementById("lastModified").innerHTML = lastModified;
+
