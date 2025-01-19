6f230d3544fd23bd4684d7d607324c448d74844f
j-w-wright <wri24011@byui.edu>
2025-01-18 22:17:02 -0700

    final commit

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,7 +1,17 @@
-const date = new Date();
-const currentYear = date.getFullYear();
+//hamburger menu
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+    navigation.classList.toggle('show');
+    hamButton.classList.toggle('show');
+});
+
+
+let date = new Date();
+let currentYear = date.getFullYear();
 document.getElementById("currentYear").innerHTML = currentYear;
 
 
-const lastModified = new Date(document.lastModified);
+let lastModified = new Date(document.lastModified);
 document.getElementById("lastModified").innerHTML = lastModified;
