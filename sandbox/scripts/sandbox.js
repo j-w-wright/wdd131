8ee78a2390ebff94ec0387c557d6695b7f4ff2d6
j-w-wright <wri24011@byui.edu>
2025-01-22 11:59:58 -0700

    commit changes for home page and css

--- sandbox/scripts/sandbox.js
+++ sandbox/scripts/sandbox.js
@@ -15,13 +15,29 @@ document.getElementById("totalApplesElement").textContent = total;*/
 
 
 
-/* Basic DOM Manipulation Activity */
-const link = document.querySelector("a");  /* select an element from the HTML file */
+/* Basic DOM Manipulation Activity
+const link = document.querySelector("a");  /* select an element from the HTML file
 const sect = document.querySelector("section");
 const para = document.createElement("p");
 para.textContent = "We hope you enjoyed the ride."
 sect.appendChild(para);
 
 link.textContent = "Mozilla Developer Network";
-link.href = "https://developer.mozilla.org";
+link.href = "https://developer.mozilla.org"; */
 
+
+
+let scores = [100, 72, 83, 94, 88, 87];
+let accumulator = 0;
+let count = 0;
+scores.forEach(score => {
+  if (score > 87) {
+    accumulator += score;
+    count ++;
+  }
+});
+if ( count > 0 ) {
+  console.log(accumulator/count); 
+} else {
+  console.log("No scores reported.");
+}
